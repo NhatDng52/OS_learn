f821e8f3631fad6e0bdc0accd2a4d288dd8c7844
Dang Tran Minh Nhat <supernhat256@gmail.com>
2025-07-06 00:12:27 +0700

    make 2 enter and exist since some solution have different code in 2 process

--- synchronization stimulator/base_case.py
+++ synchronization stimulator/base_case.py
@@ -4,7 +4,7 @@ import time
 
     
 class Base_solution:
-    def worker(self,name, sleep_time, shared_i,want, rounds):
+    def worker(self,name, sleep_time, shared_i,want, rounds,enter_critical_section= None, exit_critical_section=None):
         for _ in range(rounds):
             time.sleep(sleep_time)  # mô phỏng process này xử lý chậm
             if want == 0 and _== rounds - 1:
@@ -12,23 +12,27 @@ class Base_solution:
                 want = 1
             if want == 1:
                 # enter critical section 
-                self.enter_critical_section()
+                enter_critical_section()
                 val = shared_i.value
         
                 val += 1
                 shared_i.value = val
                 print(f"{name}: i={shared_i.value}")
-                self.exit_critical_section()
+                exit_critical_section()
                 # exit critical section 
-    def enter_critical_section(self):
+    def enter_critical_section_1(self):
         pass
-    def exit_critical_section(self):
+    def exit_critical_section_1(self):
+        pass
+    def enter_critical_section_2(self):
+        pass
+    def exit_critical_section_2(self):
         pass
     def test_mutual_exclusion(self, rounds=20000):
         "Test mutual exclusion, kết khác round x2 thì sẽ không thỏa  "
         self.shared_i = Value('i', 0, lock=False)  # lock=False để thực sự share raw
-        p1 = Process(target=self.worker, args=("P1", 0, self.shared_i,1, rounds))
-        p2 = Process(target=self.worker, args=("P2", 0, self.shared_i,1, rounds))
+        p1 = Process(target=self.worker, args=("P1", 0, self.shared_i,1, rounds, self.enter_critical_section_1, self.exit_critical_section_1))
+        p2 = Process(target=self.worker, args=("P2", 0, self.shared_i,1, rounds, self.enter_critical_section_2, self.exit_critical_section_2))
         p1.start()
         p2.start()
         p1.join()
@@ -37,8 +41,8 @@ class Base_solution:
     def test_progress(self, rounds=200):
         "Test progress, nếu bị p2 bị chặn  vì p không muốn vào critical section thì p2 sẽ print đúng 1 dòng ở cuối do p1 chuyển want ở cuối"   
         self.shared_i = Value('i', 0, lock=False)  # lock=False để thực sự share raw
-        p1 = Process(target=self.worker, args=("P1", 0, self.shared_i,0, rounds)) 
-        p2 = Process(target=self.worker, args=("P2", 0, self.shared_i,0, 1))
+        p1 = Process(target=self.worker, args=("P1", 0, self.shared_i,0, rounds, self.enter_critical_section_1, self.exit_critical_section_1)) 
+        p2 = Process(target=self.worker, args=("P2", 0, self.shared_i,1, 1     , self.enter_critical_section_2, self.exit_critical_section_2))
         p1.start()
         p2.start()
         p1.join()
@@ -46,8 +50,8 @@ class Base_solution:
     def test_bounded_waiting(self, rounds=200,sleep_time=0.01):
         "Test bounded waiting,mặc dù p2 sleep lâu hơn p1 nhưng vẫn có thể vào critical section" 
         self.shared_i = Value('i', 0, lock=False)  # lock=False để thực sự share raw
-        p1 = Process(target=self.worker, args=("P1", 0, self.shared_i,0, rounds)) 
-        p2 = Process(target=self.worker, args=("P2", sleep_time, self.shared_i,1, rounds))
+        p1 = Process(target=self.worker, args=("P1", 0         , self.shared_i,0, rounds, self.enter_critical_section_1, self.exit_critical_section_1)) 
+        p2 = Process(target=self.worker, args=("P2", sleep_time, self.shared_i,1, rounds, self.enter_critical_section_2, self.exit_critical_section_2))
         p1.start()
         p2.start()
         p1.join()
